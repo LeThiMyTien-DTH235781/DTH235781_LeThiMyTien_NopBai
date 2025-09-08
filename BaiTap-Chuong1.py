0dbc65f18bb9a323d0ea1f77f8995d311a0fd1ba
DTH235824-LeThiMyTien <tien_dth235781@student.agu.edu.vn>
2025-09-08 07:33:20 +0700

    Add files via upload

---
+++ BaiTap-Chuong1.py
@@ -0,0 +1,34 @@
+﻿#Câu 4
+print("Xin chao")
+
+#Câu 5
+print("Le Thi My Tien")
+
+#Câu 6
+print("Minh ve minh co nho ta")
+print("Muoi lam nam ay thiet tha man nong")
+print("Minh ve  minh co nho khong")
+print("Nhin cay nho nui, nhin song nho nguon")
+
+#Câu 7 
+chuoi = input("Nhap mot chuoi bat ki: ")
+print("Chuoi ban vua nhap la:", chuoi)
+
+#Câu 8
+print("****")
+print(" *  *")
+print("  *  *")
+print("   ****")
+
+#Câu 9
+stars = [9,9,9,9]
+width = max(stars)
+for s in stars:
+    print(" " * ((width - s) // 2) + "*" * s)
+
+
+#Câu 10 
+stars = [1, 3, 7, 3, 5, 11, 2, 2]
+width = max(stars)
+for s in stars:
+    print(" " * ((width - s) // 2) + "*" * s)
